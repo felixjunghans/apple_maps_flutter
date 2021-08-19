c3647bb2e07ee6d4e30a65b76b944ea16b5eb6a7
felixjunghans <kontakt@felixjunghans.de>
2021-08-19 18:36:05 +0200

    fix cluster builder

--- ios/Classes/Annotations/ClusterableAnnotationView.swift
+++ ios/Classes/Annotations/ClusterableAnnotationView.swift
@@ -8,16 +8,16 @@
 import MapKit
 
 @available(iOS 11.0, *)
-class ClusterableAnnotationView: MKAnnotationView {
+class ClusterableAnnotationView: MKMarkerAnnotationView {
 
     var lastAnnotation: FlutterAnnotation?
-
+    let label = UILabel.init(frame: CGRect(x: 0, y: 70, width: 60, height: 15))
+    
     override var annotation: MKAnnotation? {
         didSet {
             guard let mapItem = annotation as? FlutterAnnotation, mapItem != lastAnnotation else { return }
-            image = mapItem.icon.image
+            glyphImage = mapItem.image
             clusteringIdentifier = mapItem.clusteringIdentifier
-            lastAnnotation = mapItem
         }
     }
 }
@@ -34,4 +34,38 @@ final class ClusterAnnotationView: MKMarkerAnnotationView {
         super.prepareForDisplay()
         displayPriority = .required
     }
+}
+
+private func drawMarker() -> UIImage {
+    if #available(iOS 10.0, *) {
+        let size = 60.0
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: size, height: size + 10))
+        let image = renderer.image { _ in
+            // Fill full circle with wholeColor
+            UIColor.systemGreen.setFill()
+            UIBezierPath(roundedRect: CGRect(x: 0, y: 0, width: size, height: size), cornerRadius: 10).fill()
+
+            let heightWidth = 60
+            let path = UIBezierPath()
+            
+            path.move(to: CGPoint(x: size / 2 - 10, y: size))
+            path.addLine(to: CGPoint(x:heightWidth/2, y: heightWidth + 10))
+            path.addLine(to: CGPoint(x: size / 2 + 10, y: size))
+            path.addLine(to: CGPoint(x:size / 2 - 10, y: size))
+            path.close()
+            path.fill()
+
+            // Finally draw count text vertically and horizontally centered
+            let attributes = [ NSAttributedString.Key.foregroundColor: UIColor.black,
+                               NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 20)]
+            let text = "Ok"
+            let size = text.size(withAttributes: attributes)
+            let rect = CGRect(x: 20 - size.width / 2, y: 20 - size.height / 2, width: size.width, height: size.height)
+            text.draw(in: rect, withAttributes: attributes)
+        }
+        return image
+    } else {
+        // Fallback on earlier versions
+    }
+    return UIImage()
 }
